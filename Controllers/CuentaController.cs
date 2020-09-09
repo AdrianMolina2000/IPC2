f200863e9c0acd3753ed7b71af91ee9e18d7a94e
Adrian Molina <adriansmc@gmail.com>
2020-09-08 20:43:51 -0600

    Agregue archivos al repositorio

---
+++ Controllers/CuentaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IPC2.Models;
+using System.Data.SqlClient;
+
+namespace IPC2.Controllers
+{
+    public class CuentaController : Controller
+    {
+        SqlConnection connect = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        SqlDataReader rd;
+        // GET: Usuario
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            connect.ConnectionString = "data source= localhost; database= Othello; integrated security = SSPI;";
+        }
+        [HttpPost]
+
+        public ActionResult Verify(Cuenta account)
+        {
+            connectionString();
+            connect.Open();
+            command.Connection = connect;
+            command.CommandText = "select * from usuarios where nickname='"+account.Nickname+"' and contraseña= '"+account.Password+"'";
+            rd = command.ExecuteReader();
+            if(rd.Read())
+            {
+
+                connect.Close();
+                return Redirect("~/Home/Index");
+            }
+            else
+            {
+                connect.Close();
+                return RedirectToAction("Login", new { message = ("No se reconocen los datos ingresados") });
+            }
+        }
+
+    }
+}
---
+++ Models/Cuenta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPC2.Models
+{
+    public class Cuenta
+    {
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+
+    }
+}
