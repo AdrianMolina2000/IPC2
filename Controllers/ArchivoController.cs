03eede447e16cb2dfa0ca0337e4f6e592ee37fe2
Adrian Molina <adriansmc@gmail.com>
2020-09-11 21:57:05 -0600

    Tablero agregado

---
+++ Controllers/ArchivoController.cs
@@ -0,0 +1,35 @@
+﻿using IPC2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IPC2.Controllers
+{
+    public class ArchivoController : Controller
+    {
+        // GET: Archivo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Archi model)
+        {
+            string RutaSitio = Server.MapPath("~/");
+            string PathArchivo1 = Path.Combine(RutaSitio+"/files/archivo1.xml");
+            if (!ModelState.IsValid)
+
+            {
+                return View("~/Home/Index", model);
+            }
+
+            model.Archivo1.SaveAs(PathArchivo1);
+
+            return Redirect("~/Home/Index");
+        }
+    }
+}
---
+++ Models/Archi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IPC2.Models
+{
+    public class Archi
+    {
+        [Required]
+        [DisplayName("Mi Archivo")]
+        public HttpPostedFileBase Archivo1 { get; set; }
+    }
+}
