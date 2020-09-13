c49703152bcef37712045140946e1f9532c7581b
Adrian Molina <adriansmc@gmail.com>
2020-09-12 21:44:29 -0600

    Ultimos cambios

--- Controllers/JugarController.cs
+++ Controllers/JugarController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using IPC2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace IPC2.Controllers
 {
@@ -13,6 +15,51 @@ namespace IPC2.Controllers
         {
 
             return View();
+        }
+
+        public ActionResult Cargar()
+        {
+            if (TempData["tableroData"] == null)
+            {
+                ViewBag.ShowList = false;
+                return View();
+            }
+            else
+            {
+                List<Fichas> empList = (List<Fichas>)TempData["tableroData"];
+                ViewBag.ShowList = true;
+                return View(empList);
+            }
+        }
+        [HttpPost]
+        public ActionResult UploadXML()
+        {
+            try
+            {
+                List<Fichas> empList = new List<Fichas>();
+                var xmlFile = Request.Files[0];
+                if (xmlFile != null && xmlFile.ContentLength > 0)
+                {
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(xmlFile.InputStream);
+                    XmlNodeList empNodes = xmlDocument.SelectNodes("tablero/ficha");
+                    foreach (XmlNode emp in empNodes)
+                    {
+                        empList.Add(new Fichas()
+                        {
+                            color = emp["color"].InnerText,
+                            columna = emp["columna"].InnerText,
+                            fila = emp["fila"].InnerText,
+                        });
+                    }
+                    TempData["tableroData"] = empList;
+                }
+                return RedirectToAction("Cargar");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Cargar");
+            }
         }
     }
 }
---
+++ Models/Fichas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPC2.Models
+{
+    public class Fichas
+    {
+        public string color { get; set; }
+        public string columna { get; set; }
+        public string fila { get; set; }
+    }
+}
